43f12f95f8a73b3dcf4cd2278ba8c254d3f21e51
hamapu <hamapu@gmail.com>
2019-10-01 02:43:59 +0900

    Revert "refs #3 これかな？"
    This reverts commit dbe41fc9d65b060f4d2a4e7d0d64383c25e2a8ea.

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,9 @@
 ActiveRecord::Schema.define(version: 2019_09_18_012131) do
 
   create_table "issues", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title"
     t.text "body"
-    t.integer "kind", default: 3
-    t.integer "del_flg", default: 0, null: false
+    t.integer "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
